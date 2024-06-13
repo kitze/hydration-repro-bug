a32cb940123a90bf706ed743332b41900ce74a35
Kitze <kristijan.mkd@gmail.com>
2024-06-13 11:34:03 +0200

    dark light switch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import { Button, Group } from "@mantine/core";
+import {DarkLightSwitch} from "../DarkLightSwitch";
 
 export default function IndexPage() {
   return (
     <Group mt={50} justify="center">
+
       <Button size="xl">Welcome to Mantine!</Button>
+        <DarkLightSwitch/>
     </Group>
   );
 }
